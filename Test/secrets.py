ba5a2c4393d22593eab47da920dbcafad5d2b2b0
joelesim <82829045+joelesim@users.noreply.github.com>
2021-04-20 02:28:14 +0800

    Delete Test directory

--- Test/secrets.py
+++
@@ -1,5 +0,0 @@
-key = ['QRcpElmMIzUlLCxtCQk7SSCv1',
-      '0wwWynjJ5vfyUyocoKfVS8qouUlNf0zKlkAeD5b8uJ1muynsQr',
-      '1294976905607700480-cbGCrGwmbJQnyLHsNAEczNQID4iFFt',
-      '5uvomYqgc6gBVv28ZDL28i2tAZBRu8rM5gqMz2grRLxma'
-]
